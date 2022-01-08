738233978f99baeb78a959403f684b3f10e4c234
none44353 <1124536810@qq.com>
2022-01-08 17:56:51 +0800

    AutoBuild

--- scratch/reno.cc
+++ scratch/reno.cc
@@ -46,6 +46,7 @@
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/traffic-control-module.h"
+#include <bits/stdc++.h>
 
 using namespace ns3;
 std::string dir = "results/";
@@ -98,7 +99,8 @@ TraceCwnd (uint32_t node, uint32_t cwndWindow,
 /*void InstallBulkSend (Ptr<Node> node, Ipv4Address address, uint16_t port, std::string socketFactory,
                       uint32_t nodeId, uint32_t cwndWindow,
                       Callback <void, uint32_t, uint32_t> CwndTrace)*/
-void InstallBulkSend (Ptr<Node> node, Ipv4Address address, uint16_t port, std::string socketFactory)
+
+void InstallBulkSend (Ptr<Node> node, Ipv4Address address, uint16_t port, std::string socketFactory) //TCP only
 {
   BulkSendHelper source (socketFactory, InetSocketAddress (address, port));
   source.SetAttribute ("MaxBytes", UintegerValue (0)); //不知道这个参数是干嘛的
@@ -109,6 +111,23 @@ void InstallBulkSend (Ptr<Node> node, Ipv4Address address, uint16_t port, std::s
   sourceApps.Stop (stopTime);
 }
 
+void InstallOnOffSend (Ptr<Node> node, Ipv4Address address, uint16_t port, std::string socketFactory, 
+                      std::string onTime, std::string offTime, uint64_t payloadSize, std::string dataRate)
+{
+  OnOffHelper onoff (socketFactory, InetSocketAddress (address, port));
+  onoff.SetAttribute ("OnTime",  StringValue(onTime));
+  onoff.SetAttribute ("OffTime", StringValue(offTime));
+  onoff.SetAttribute ("PacketSize", UintegerValue(payloadSize));
+  onoff.SetAttribute ("DataRate", StringValue(dataRate)); //bit/s
+  ApplicationContainer onoffApps = onoff.Install (node);
+  onoffApps.Start (Seconds (10.0));
+  onoffApps.Stop (stopTime);
+
+  //Simulator::Schedule (Seconds (10.0) + Seconds (0.001), &TraceCwnd, nodeId, cwndWindow, CwndTrace);
+  //把定时跟踪输出去掉
+}
+
+
 // Function to install sink application
 void InstallPacketSink (Ptr<Node> node, uint16_t port, std::string socketFactory)
 {
@@ -118,9 +137,82 @@ void InstallPacketSink (Ptr<Node> node, uint16_t port, std::string socketFactory
   sinkApps.Stop (stopTime);
 }
 
+LosslessOnoffTable* globalOnoffTable;
+
+int n, m;
+NodeContainer nodes;
+std::vector <NetDeviceContainer> netDevices;
+std::vector <PointToPointHelper> channelHelpers;
+std::vector <Ipv4InterfaceContainer> IPAddresses;
+InternetStackHelper internetStack;
+TrafficControlHelper tch;
+QueueDiscContainer qd;
+std::vector <std::string> ipBase;
+std::vector <std::string> ipMask;
+
+std::string qdiscTypeId = "ns3::LosslessQueueDisc"; 
+//这个是装在TC Layer上的队列type, 现在把它改成on-off model的，
+
+//我们要新造一个qdiscType，它需要用另一种和routing包平行的包来和相邻的router交流堵塞信息，并据此更新自己的路由表。
+//特别地，我们是lossless network，所以不能Drop包，只能Congest包。这个应该有封装得比较好的函数可以用来做。
+
+
+void build(std::string filename) {
+  std::freopen(filename.c_str(), "r", stdin);
+  std::cin >> n >> m;
+  // Create nodes
+  nodes.Create(n);
+
+  // Build Channels
+  for (int i = 0; i < m; ++i) {
+    int x, y;
+    std::cin >> x >> y;
+    std::string DataRate, Delay, Address, Mask;
+    std::cin >> DataRate >> Delay >> Address >> Mask;
+    // Create the point-to-point link helpers and connect two nodes
+    PointToPointHelper p2ph;
+    p2ph.SetDeviceAttribute    ("DataRate", StringValue (DataRate));
+    p2ph.SetChannelAttribute   ("Delay", StringValue (Delay));
+    netDevices.push_back(p2ph.Install(nodes.Get(x), nodes.Get(y)));
+    channelHelpers.push_back(p2ph);
+
+    ipBase.push_back(Address);
+    ipMask.push_back(Mask);
+  }
+
+  std::fclose(stdin);
+  internetStack.Install (nodes); // Add one more device to each node
+
+  // Assign IP addresses to all the network devices
+  for (int i = 0; i < m; ++i) {
+    Ipv4AddressHelper ipah (ipBase[i].c_str(), ipMask[i].c_str());
+    IPAddresses.push_back(ipah.Assign (netDevices[i]));
+  }
+
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  // Set default parameters for queue discipline
+  Config::SetDefault (qdiscTypeId + "::MaxSize", QueueSizeValue (QueueSize ("100p")));
+
+  // Install queue discipline on router
+  tch.SetRootQueueDisc (qdiscTypeId);
+  for (int i = 0; i < n; ++i) {
+    Ptr<ns3::Node> node = nodes.Get(i);
+    uint32_t num = node -> GetNDevices();
+    for (uint32_t k = 0; k + 1 != num; ++k) {
+      Ptr<NetDevice> dev = node -> GetDevice (k);
+      tch.Uninstall (dev);
+      qd.Add (tch.Install (dev, globalOnoffTable).Get (0));
+    }
+  }
+
+  // Enable BQL
+  tch.SetQueueLimits ("ns3::DynamicQueueLimits");
+}
+
 int main (int argc, char *argv[])
 {
-  LosslessOnoffTable* globalOnoffTable = new LosslessOnoffTable();
+  globalOnoffTable = new LosslessOnoffTable();
   globalOnoffTable->globalInit();
 
   uint32_t stream = 1;
@@ -129,24 +221,20 @@ int main (int argc, char *argv[])
   
   //std::string tcpTypeId = "ns3::TcpLinuxReno"; //这个是拥塞控制算法对应的组件名称，它装载在TcpL4Protocol::SocketType上，
 
-  
+  std::string topologyFile = "scratch/topo.txt";
   //需要详细观察代码看它是怎么被调用的，并新建一个符合我们要求的tcpType，它要根据routing table上的管子有没有堵住来决定发还是不发，
   //写完后我们要把这个拥塞控制算法换成修改后的tcpType
 
-  std::string qdiscTypeId = "ns3::LosslessQueueDisc"; 
-  //这个是装在TC Layer上的队列type, 现在把它改成on-off model的，
-
-  //我们要新造一个qdiscType，它需要用另一种和routing包平行的包来和相邻的router交流堵塞信息，并据此更新自己的路由表。
-  //特别地，我们是lossless network，所以不能Drop包，只能Congest包。这个应该有封装得比较好的函数可以用来做。
 
   //根据TCD的构想，网络内部的情况，绑在包上，ns3的packet类有个Tag属性，可以通过打Tag来把拥塞标记弄到包上，在网络边缘检测tag标记修改拥塞控制
-  bool isSack = true;
+  //bool isSack = true;
   //uint32_t delAckCount = 1;
   //std::string recovery = "ns3::TcpClassicRecovery"; 
 
   CommandLine cmd;
   //cmd.AddValue ("tcpTypeId", "TCP variant to use (e.g., ns3::TcpNewReno, ns3::TcpLinuxReno, etc.)", tcpTypeId);
   cmd.AddValue ("qdiscTypeId", "Queue disc for gateway (e.g., ns3::CoDelQueueDisc)", qdiscTypeId);
+  cmd.AddValue ("topologyFile", "Path to the file describing the topology structure you need (e.g., scratch/topo.txt)", topologyFile);
   //cmd.AddValue ("segmentSize", "TCP segment size (bytes)", segmentSize);
   //cmd.AddValue ("delAckCount", "Delayed ack count", delAckCount);
   //cmd.AddValue ("enableSack", "Flag to enable/disable sack in TCP", isSack);
@@ -174,71 +262,6 @@ int main (int argc, char *argv[])
       Config::SetDefault ("ns3::TcpL4Protocol::SocketType", TypeIdValue (TypeId::LookupByName (tcpTypeId)));
     }*/
 
-  // Create nodes
-  NodeContainer leftNodes, rightNodes, routers;
-  routers.Create (2);
-  leftNodes.Create (1);
-  rightNodes.Create (1);
-
-  std::vector <NetDeviceContainer> leftToRouter;
-  std::vector <NetDeviceContainer> routerToRight;
-
-  // Create the point-to-point link helpers and connect leaf nodes to router
-  PointToPointHelper pointToPointLeft;
-  pointToPointLeft.SetDeviceAttribute    ("DataRate", StringValue ("10Mbps"));
-  pointToPointLeft.SetChannelAttribute   ("Delay", StringValue ("1ms"));
-  leftToRouter.push_back (pointToPointLeaf.Install (leftNodes.Get (0), routers.Get (0)));
-
-  // Create the point-to-point link helpers and connect two router nodes
-  PointToPointHelper pointToPointRouter;
-  pointToPointRouter.SetDeviceAttribute  ("DataRate", StringValue ("100bps"));
-  pointToPointRouter.SetChannelAttribute ("Delay", StringValue ("10ms"));
-  NetDeviceContainer r1r2ND = pointToPointRouter.Install (routers.Get (0), routers.Get (1));
-
-  
-  PointToPointHelper pointToPointRight;
-  pointToPointRight.SetDeviceAttribute    ("DataRate", StringValue ("2Mbps"));
-  pointToPointRight.SetChannelAttribute   ("Delay", StringValue ("1ms"));
-  routerToRight.push_back (pointToPointRight.Install (routers.Get (1), rightNodes.Get (0)));
-
-  InternetStackHelper internetStack;
-
-  internetStack.Install (leftNodes);
-  internetStack.Install (rightNodes);
-  internetStack.Install (routers);
-
-  // Assign IP addresses to all the network devices
-  Ipv4AddressHelper ipAddresses ("10.0.0.0", "255.255.255.0");
-
-  Ipv4InterfaceContainer r1r2IPAddress = ipAddresses.Assign (r1r2ND);
-  ipAddresses.NewNetwork ();
-
-  std::vector <Ipv4InterfaceContainer> leftToRouterIPAddress;
-  leftToRouterIPAddress.push_back (ipAddresses.Assign (leftToRouter [0]));
-  ipAddresses.NewNetwork ();
-
-  std::vector <Ipv4InterfaceContainer> routerToRightIPAddress;
-  routerToRightIPAddress.push_back (ipAddresses.Assign (routerToRight [0]));
-
-  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-
-/*
-  // Set default sender and receiver buffer size as 1MB
-  Config::SetDefault ("ns3::TcpSocket::SndBufSize", UintegerValue (1 << 20));
-  Config::SetDefault ("ns3::TcpSocket::RcvBufSize", UintegerValue (1 << 20));
-
-  // Set default initial congestion window as 10 segments
-  Config::SetDefault ("ns3::TcpSocket::InitialCwnd", UintegerValue (10));
-
-  // Set default delayed ack count to a specified value
-  Config::SetDefault ("ns3::TcpSocket::DelAckCount", UintegerValue (delAckCount));
-
-  // Set default segment size of TCP packet to a specified value
-  Config::SetDefault ("ns3::TcpSocket::SegmentSize", UintegerValue (segmentSize));
-
-  // Enable/Disable SACK in TCP
-  Config::SetDefault ("ns3::TcpSocketBase::Sack", BooleanValue (isSack));
-*/
 
   // Create directories to store dat files
   struct stat buffer;
@@ -260,30 +283,10 @@ int main (int argc, char *argv[])
  // NS_ASSERT_MSG (retVal == 0, "Error in return value");
   NS_UNUSED (retVal);
 
-  // Set default parameters for queue discipline
-  Config::SetDefault (qdiscTypeId + "::MaxSize", QueueSizeValue (QueueSize ("100p")));
+  std::cout << "cout test\n";
+  build(topologyFile);
 
   std::cout << "cout test\n";
-
-  // Install queue discipline on router
-  TrafficControlHelper tch;
-  tch.SetRootQueueDisc (qdiscTypeId);
-  QueueDiscContainer qd;
-  tch.Uninstall (leftNodes.Get (0)->GetDevice (0));
-  qd.Add (tch.Install (leftNodes.Get (0)->GetDevice (0), globalOnoffTable).Get (0));
-  tch.Uninstall (routers.Get (0)->GetDevice (0));
-  qd.Add (tch.Install (routers.Get (0)->GetDevice (0), globalOnoffTable).Get (0));
-  tch.Uninstall (routers.Get (0)->GetDevice (1));
-  qd.Add (tch.Install (routers.Get (0)->GetDevice (1), globalOnoffTable).Get (0));
-  tch.Uninstall (routers.Get (1)->GetDevice (0));
-  qd.Add (tch.Install (routers.Get (1)->GetDevice (0), globalOnoffTable).Get (0));
-  tch.Uninstall (routers.Get (1)->GetDevice (1));
-  qd.Add (tch.Install (routers.Get (1)->GetDevice (1), globalOnoffTable).Get (0));
-  tch.Uninstall (rightNodes.Get (0)->GetDevice (0));
-  qd.Add (tch.Install (rightNodes.Get (0)->GetDevice (0), globalOnoffTable).Get (0));
-
-  // Enable BQL
-  tch.SetQueueLimits ("ns3::DynamicQueueLimits");
 
   // Calls function to check queue size
   Simulator::ScheduleNow (&CheckQueueSize, qd.Get (0));
@@ -297,17 +300,20 @@ int main (int argc, char *argv[])
 
   // Install packet sink at receiver side
   uint16_t port = 50000;
-  InstallPacketSink (rightNodes.Get (0), port, socketFactory);
+  InstallPacketSink (nodes.Get (n-1), port, socketFactory);
 
   // Install BulkSend application
   //InstallBulkSend (leftNodes.Get (0), routerToRightIPAddress [0].GetAddress (1), port, socketFactory, 2, 0, MakeCallback (&CwndChange));
-  InstallBulkSend (leftNodes.Get (0), routerToRightIPAddress [0].GetAddress (1), port, socketFactory);
+  //InstallBulkSend (leftNodes.Get (0), routerToRightIPAddress [0].GetAddress (1), port, socketFactory);
+  InstallOnOffSend (nodes.Get (0), IPAddresses [m - 1].GetAddress (1), port, socketFactory, 
+                    "ns3::ConstantRandomVariable[Constant=1]", "ns3::ConstantRandomVariable[Constant=0]", 
+                    1024, "50Mbps");
+
 
   // Enable PCAP on all the point to point interfaces
-  pointToPointLeft.EnablePcapAll (dir + "pcap/ns-3", true);
-  pointToPointRight.EnablePcapAll (dir + "pcap/ns-3", true);
-
-
+  channelHelpers[0].EnablePcapAll (dir + "pcap/ns-3", true);
+  channelHelpers[m -1].EnablePcapAll (dir + "pcap/ns-3", true);
+  
   Simulator::Stop (stopTime);
   Simulator::Run ();
 
